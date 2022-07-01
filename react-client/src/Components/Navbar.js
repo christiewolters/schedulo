90671ab297094f77233a0b26f7cb055d1d4bd335
Christie Wolters <christiewolters@gmail.com>
2022-07-01 10:00:21 -0700

    Navbar complete

--- react-client/src/Components/Navbar.js
+++ react-client/src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import React, { useContext } from 'react';
 import AuthContext from '../AuthContext';
 
@@ -8,51 +8,54 @@ function Navbar() {
 
     return (
         <>
-<nav className="navbar navbar-inverse" role="navigation">
-                  <div className="container-fluid">
+            <nav className="navbar navbar-inverse" role="navigation">
+                <div className="container-fluid">
                     <div className="navbar-header">
-                      <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-7">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                      </button>
-                      <a className="navbar-brand" href="#">Shift Scheduler</a>
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-7">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">Shift Scheduler</a>
                     </div>
                     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-7">
-                      <p className="navbar-text navbar-right">Signed in as {auth.user.username} {auth.employee.firstName}. <a className="navbar-link" href="">Sign out.</a></p>
+
+
+                        <ul className="nav navbar-nav">
+
+                            <li className={(window.location.pathname == "/" ? "active" : "")} ><NavLink to="/" activeClassName="active">My Shifts</NavLink></li>
+
+                            {auth.user.hasRole("ROLE_EMPLOYEE") && (
+                                <>
+                                    <li className={(window.location.pathname == "/employee/availability" ? "active" : "")}><Link to="/employee/availability">My Availability</Link></li>
+                                </>
+                            )}
+
+                            {auth.user.hasRole("ROLE_MANAGER") && (
+                                <li  className={(window.location.pathname == "/manager/schedules" ? "active" : "")}><Link to="/manager/schedules">Schedules</Link></li>
+                            )}
+
+                        </ul>
+
+
+
+                        {auth.user && (
+                            <p className="navbar-text navbar-right">
+                                Signed in as {auth.employee.firstName} {auth.employee.lastName}.
+                                <a className="navbar-link" href="" onClick={() => auth.logout()}> Sign out.</a>
+                            </p>
+                        )}
+
+                        {!auth.user && (
+                            <p>
+                                <Link to="/login">Dave. You shouldn't be here right now.</Link>
+                            </p>
+                        )}
+
                     </div>
-                  </div>
-                </nav>
-
-
-
-            <ul className="collapse navbar-collapse" id="bs-example-navbar-collapse-7" >
-                <li><Link to="/">My Shifts</Link></li>
-                {auth.user.hasRole("ROLE_EMPLOYEE") && (
-                    <>
-                        <li><Link to="/employee/availability">My Availability</Link></li>
-                    </>
-                )}
-
-                {auth.user.hasRole("ROLE_MANAGER") && (
-                    <li><Link to="/manager/schedules">Schedules</Link></li>
-                )}
-
-                {auth.user && (
-                    <>
-                        <li>{auth.user.username}</li>
-                        <li><button onClick={() => auth.logout()}>Logout</button></li>
-                    </>
-                )}
-
-                {!auth.user && (
-                    <>
-                        <li><Link to="/login">Dave. You shouldn't be here right now.</Link></li>
-                    </>
-                )}
-
-            </ul>
+                </div>
+            </nav>
         </>
     );
 }

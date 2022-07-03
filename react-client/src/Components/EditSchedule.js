c2014165f59e5444a7c3984c3c6d4c304f5ace37
Christie Wolters <christiewolters@gmail.com>
2022-07-03 02:58:42 -0700

    Availability broke? Getting 404 errors

--- react-client/src/Components/EditSchedule.js
+++ react-client/src/Components/EditSchedule.js
@@ -12,8 +12,14 @@ function EditSchedule() {
     let schedule = null;
     let employees = [];
     let shifts = [];
+    let availabilities = [];
     let dateList = [];
+    const [errors, setErrors] = useState([]);
 
+    //Add form
+    const [startTime, setStartTime] = useState('');
+    const [endTime, setEndTime] = useState('');
+    const [availableEmployees, setAvailableEmployees] = useState([]);
 
 
     async function functionName() {
@@ -23,31 +29,13 @@ function EditSchedule() {
         console.log("shifts : " + JSON.stringify(shifts));
         await getEmployees();
         console.log("employees : " + employees);
+        await getAvailabilities();
+        console.log("availabilities : " + availabilities);
         console.log("Done");
         loadTable();
     }
-
     functionName().then(console.log("Actually done."));
 
-    // useEffect(() => {
-    //     getSchedule()
-    //         .then(console.log("schedule : " + JSON.stringify(schedule)))
-    //         .then(makeDateList())
-    //         .then(console.log("dateList: " + JSON.stringify(dateList)))
-    //         .then(getShifts())
-    //         .then(console.log("shifts : " + JSON.stringify(shifts)))
-    //         .then(getEmployees())
-    //         .then(console.log("employees : " + employees))
-    //         .then(loadTable())
-    //         .catch(
-    //             console.log("didn't work")
-    //         );
-    // }, [scheduleId]);
-
-    // useEffect(() => {
-    //     getShifts()
-    //         .then(loadTable())
-    // }, [shifts.length])
 
 
     //FIRST, get scheduling dates using id in the browser path
@@ -68,7 +56,7 @@ function EditSchedule() {
                     }
                 })
                 .then(data => { schedule = data })
-                .then(data => {makeDateList(data)})
+                .then(data => { makeDateList(data) })
                 .catch(console.log);
         }
     };
@@ -86,10 +74,10 @@ function EditSchedule() {
         return dateArray;
     };
 
-
+    //Builds and displays HTML data
     function loadTable() {
         console.log("entered loadTable");
-        if( schedule === null || shifts.lenth === 0 || employees.length === 0 ){
+        if (schedule === null || shifts.lenth === 0 || employees.length === 0) {
             console.log("Couldn't load.");
             return;
         }
@@ -105,13 +93,13 @@ function EditSchedule() {
 
         const tableBody = document.getElementById("tableBody");
         let bodyHtml = "";
-        for(const employee of employees){
+        for (const employee of employees) {
             bodyHtml += `<tr><td>${employee.firstName} ${employee.lastName}</td>`;
 
-            for(const currDate of dates){
+            for (const currDate of dates) {
                 bodyHtml += "<td>";
                 let currShifts = shifts.filter(shift => shift.employeeId === employee.employeeId && date.isSameDay(currDate, new Date(shift.startTime)));
-                for(const thisShift of currShifts){
+                for (const thisShift of currShifts) {
                     bodyHtml += `${date.format(thisShift.startTime, 'h:mm A')} - ${date.format(thisShift.endTime, 'h:mm A')}`;
                 }
                 bodyHtml += "</td>";
@@ -124,7 +112,7 @@ function EditSchedule() {
 
 
 
-    //SECOND, get a list of all employees at the company
+    //Get a list of all employees at the company
     async function getEmployees() {
         const init = {
             headers: {
@@ -144,6 +132,24 @@ function EditSchedule() {
     };
 
 
+    //Get a list of all availabilities
+    async function getAvailabilities() {
+        const init = {
+            headers: {
+                'Authorization': `Bearer ${auth.user.token}`
+            },
+        };
+        await fetch(`http://localhost:8080/api/availabilities`, init)
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    return Promise.reject(`Unexpected status code: ${response.status}`);
+                }
+            })
+            .then(data => availabilities = data)
+            .catch(console.log);
+    };
 
 
 
@@ -172,128 +178,65 @@ function EditSchedule() {
     };
 
 
-
-    // //DISPLAY on the page
-    // const renderShifts = (employee) => {
-    //     let employeeId = employee.employeeId;
-    //     console.log("rendering...");
-    //     const tableRow = document.getElementById("employee" + employeeId);
-    //     if (tableRow === null) { return; }
-    //     let html = `<td>{employee.firstName} {employee.lastName}</td>`;
-    //     let thisEmployeeShifts = shifts.filter(shift => shift.employeeId = employeeId);
-    //     let somedates = dateList(schedule);
-    //     console.log("somedates : " + somedates);
-    //     console.log("somedates again : " + JSON.stringify(somedates));
-    //     for (const currDate of somedates) {
-    //         console.log(currDate instanceof Date);
-    //         console.log(currDate);
-    //         html += "<td>";
-    //         let shifts = thisEmployeeShifts.filter(shift => date.isSameDay(currDate, new Date(shift.startTime)));
-    //         for (const s of shifts) {
-
-    //             html += `${date.format(s.startTime, 'h:mm A')} - ${date.format(s.endTime, 'h:mm A')}`;
-
-    //         }
-    //         html += "</td>";
-    //     }
-    //     tableRow.innerHTML = html;
-    // }
-
-    // function testFunction(event){
-    //     console.log("Running test function");
-    //     getShiftsByEmployee(6).then(shifts => (
-    //         shifts.map();
-    //     ));
-    // }
-
-    // function getShiftsByEmployee(employeeId) {
-    //     console.log("employee id : " + employeeId);
-    //     let url = `http://localhost:8080/api/shifts/schedule/${scheduleId}/${employeeId}`;
-    //     const init = {
-    //         method: "GET",
-    //         headers: {
-    //             'Authorization': `Bearer ${auth.user.token}`
-    //         }
-    //     };
-    //     return fetch(url, init)
-    //         .then(response => {
-    //             if (response.status === 200) {
-    //                 return response.json();
-    //             }
-    //         }).then(data => {
-    //             if(!data) { console.log("shift list not found");
-    //                 return ["not found"];}
-    //         })
-    //         .catch(console.log);
-    // }
-
-    // //THIRD, get employee shifts
-    // const getEmployeeShifts = (employee) => {
-    //     const init = {
-    //         headers: {
-
-    //         },
-    //     };
-
-    //     fetch(`http://localhost:8080/api/shifts/schedule/${scheduleId}/${employee.employeeId}`, init)
-    //         .then(response => {
-    //             if (response.status === 200) {
-    //                 return response.json();
-    //             } else {
-    //                 return Promise.reject(`Unexpected status code: ${response.status}`);
-    //             }
-    //         })
-    //         .then(data => {
-    //             if (!data) { return null; }
-    //         })
-    //         .catch(console.log);
-    // }
-
+    //Select Employee options on add
+    useEffect(() => {
+        const selectList = document.getElementById('employeeIdForm');
+        let html = "";
+        let whoAvail = availabilities.filter(a => 
+            (new Date(a.startTime) <= new Date(startTime)) && (new Date(a.endTime) <= new Date(endTime))
+        );
+        for (const employee of employees) {
+            html +=`<option value='${employee.employee.id} '`;
+            if( whoAvail.map(a => a.employeeId).includes(employee.employeeId) ){
+                html += `className="grayed-out" `;
+            }
+            html += `>${employee.firstName} ${employee.lastName}</option>`;
+        }
+    },[startTime, endTime])
 
 
     return (
         <>
             <h3 className="text-center">Edit Schedule</h3>
 
+            {errors.length > 0 &&
+                <div className="alert alert-danger">
+                    <button type="button" className="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <strong>Oh snap! We couldn't do that! </strong>{errors.map(error => (<span key={error}>
+                        {error}. </span>
+                    ))}
+                </div>
+            }
+
+            <form id="addForm">
+                <fieldset><legend>Add Shift</legend>
+                    <label htmlFor="startTime">Starting:</label>
+                    <input type="datetime-local" className="form-control inline" id="startTime" name="startTime" onChange={(event) => setStartTime(event.target.value)}></input>
+                    <label htmlFor="endTime">Finishing:</label>
+                    <input type="datetime-local" className="form-control inline" id="startTime" name="startTime" onChange={(event) => setEndTime(event.target.value)}></input>
+                    <label htmlFor="employeeIdForm">Employee:</label>
+                    <select id="employeeIdForm" name="employeeIdForm" value="employee">
+                    </select>
+
+
+
+
+                    <button className="blue largebutton" onClick="">Register</button>
+                </fieldset>
+            </form>
+
             <table>
                 <thead>
                     <tr id="tableHead">
-                        <th>Employees</th>
-                        {/* {schedule &&
-                            dateList(schedule).map(thisDate => (
-                                <th key={JSON.stringify(thisDate)}>{date.format(thisDate, 'ddd, MMM D, YYYY')}</th>
-                            ))} */}
                     </tr>
                 </thead>
                 <tbody id="tableBody">
-                    {/* {employees &&
-                        employees.map(employee => (
-                            <tr id={"employee" + employee.employeeId} key={employee.employeeId}></tr>
-                        ))
-                    }
-
-                    {shifts &&
-                        shifts.map(shift => (
-                            <tr key={shift.shiftId}>
-                                <td>{shift.startTime.toString()} {shift.endTime.toString()}</td>
-                            </tr>
-                        ))
-                    } */}
 
                 </tbody>
 
 
 
             </table>
-
-
-
-
-
-
-
-
-
         </>
     );
 }
